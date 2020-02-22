38421f0615af276079873d6d8ed70d087a482a7c
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-02-22 11:51:51 +0200

    Chapter 4: custom  helpers

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,37 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-  
+ 
   test 'should get root path' do
     get root_path
     assert_response :success
   end
 
-  test "should get home page" do
+  test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
   
   test "should get help page" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
     
   test "should get about page" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
-  test 'should get contact page' do
+  test "should get contact page" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
