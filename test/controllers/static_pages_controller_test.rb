2fd4323e33e5d951615952a964901ce841d6ba00
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-02-24 12:33:01 +0200

    Chapter 5 Named routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,25 +8,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
   
   test "should get help page" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
     
   test "should get about page" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact page" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
