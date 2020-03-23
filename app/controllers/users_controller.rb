ae9d87b9c62bb636f0923c889217693c72f7e9cd
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-03-23 12:23:42 +0200

    Chapter 11: Activating the account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,13 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    # OPTIMIZE improve the code below
+    redirect_to root_url and return unless @user.activated?
   end
   
   def new
@@ -19,7 +21,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."   
       redirect_to root_url
     else
