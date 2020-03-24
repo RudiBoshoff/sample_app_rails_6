0d8f1862503b8adeee73c68a6171875850155e16
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-03-24 16:00:14 +0200

    Chapter 12: updating the reset complete

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -20,4 +23,55 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    # cases: 
+    # 1. An expired password_reset
+    # 2. a failed update due to invalid password
+    # 3. a failed update (which looks successful) but password is empty
+    # 4. a successful update
+
+    if params[:user][:password].empty?
+      # case 3
+      @user.errors.add(:password, "can't be empty!")
+      render 'edit'
+    elsif @user.update(user_params)
+      # case 4
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      # case 2
+      render 'edit'
+    end
+
+  end
+  
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # BEFORE FILTERS
+
+    # Find the user in the database using the email passed through params
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms that the user is valid
+    def valid_user
+      unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+        redirect_to root_url
+      end
+    end
+
+    # checks if the reset_token has expired
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -78,6 +78,11 @@ class User < ApplicationRecord
         UserMailer.password_reset(self).deliver_now
     end
 
+    # returns true if a password reset has expired
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+
     private
 
         # Converts email to lower case
